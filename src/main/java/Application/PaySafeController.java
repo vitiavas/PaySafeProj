562911a81966a9830a1ab3ace0582eda2bb1a018
vvasylkovskyi <vvasylkovskyi@eservicios.indra.es>
2018-11-24 20:19:15 +0000

    Digital Signature + Encryption

--- src/main/java/Application/PaySafeController.java
+++ src/main/java/Application/PaySafeController.java
@@ -1,126 +1,106 @@
 package Application;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.Security;
+import java.security.SignatureException;
 import java.security.UnrecoverableKeyException;
-import java.security.cert.CertificateEncodingException;
 import java.security.cert.CertificateException;
-import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
-import java.util.Collection;
 
-import org.bouncycastle.cms.CMSAlgorithm;
-import org.bouncycastle.cms.CMSEnvelopedData;
-import org.bouncycastle.cms.CMSEnvelopedDataGenerator;
 import org.bouncycastle.cms.CMSException;
-import org.bouncycastle.cms.CMSProcessableByteArray;
-import org.bouncycastle.cms.CMSTypedData;
-import org.bouncycastle.cms.KeyTransRecipientInformation;
-import org.bouncycastle.cms.RecipientInformation;
-import org.bouncycastle.cms.jcajce.JceCMSContentEncryptorBuilder;
-import org.bouncycastle.cms.jcajce.JceKeyTransEnvelopedRecipient;
-import org.bouncycastle.cms.jcajce.JceKeyTransRecipient;
-import org.bouncycastle.cms.jcajce.JceKeyTransRecipientInfoGenerator;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.bouncycastle.operator.OutputEncryptor;
+import org.bouncycastle.operator.OperatorCreationException;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.twilio.Twilio;
-import com.twilio.rest.api.v2010.account.Message;
-import com.twilio.type.PhoneNumber;
+import Crypto.CryptoUtil;
 
 @RestController
 @RequestMapping("dashboard")
 public class PaySafeController {
 
-
-    public static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
-    public static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
     
-    @RequestMapping(value = "/test", method = RequestMethod.GET)
-    public void test() {
-    	System.out.println("TEST");
-    }
-    
-
-    public X509Certificate getCertificate() throws CertificateException, NoSuchProviderException, FileNotFoundException {
-    	CertificateFactory certFactory = CertificateFactory.getInstance("X.509", "BC");
-		X509Certificate certificate = (X509Certificate) certFactory.generateCertificate(new FileInputStream("Certificate.cer"));
-		return certificate;    	
-    }
     @RequestMapping("sendMessage")
     public void sendMessage(@RequestParam(value = "senderNumber") String senderNumber,
 			@RequestParam(value = "receiverNumber") String receiverNumber,
-			@RequestParam(value = "amount") String amount) throws CertificateException, NoSuchProviderException, KeyStoreException, NoSuchAlgorithmException, IOException, UnrecoverableKeyException, CMSException {
-     
+			@RequestParam(value = "amount") String amount) throws CertificateException, NoSuchProviderException, KeyStoreException, NoSuchAlgorithmException, IOException, UnrecoverableKeyException, CMSException, OperatorCreationException, InvalidKeyException, SignatureException, InvalidAlgorithmParameterException {
+    	
     	// MAXIMIZE Key Size if necessary
     	int maxKeySize = javax.crypto.Cipher.getMaxAllowedKeyLength("AES");
     	System.out.println("Max Key Size for AES : " + maxKeySize);
     	
     	Security.addProvider(new BouncyCastleProvider());
-    	X509Certificate certificate = getCertificate();
-    	  
-    	char[] keystorePassword = "password".toCharArray();
-    	char[] keyPassword = "password".toCharArray();
-    	  
-    	KeyStore keystore = KeyStore.getInstance("PKCS12");
-    	keystore.load(new FileInputStream("Baeldung.p12"), keystorePassword);
-    	PrivateKey privateKey = (PrivateKey) keystore.getKey("baeldung", keyPassword);
-    	
-    	// TEST Encrypt/Descrypt Working
-    	String secretMessage = "My password is 123456Seven";
+    	X509Certificate certificate = CryptoUtil.getCertificate();
+    	      	 
+    	String secretMessage = senderNumber + " " + receiverNumber + " " + amount;
     	System.out.println("Original Message : " + secretMessage);
     	byte[] stringToEncrypt = secretMessage.getBytes();
-    	byte[] encryptedData = encryptData(stringToEncrypt, certificate);
-    	System.out.println("Encrypted Message : " + new String(encryptedData));
-    	byte[] rawData = decryptData(encryptedData, privateKey);
-    	String decryptedMessage = new String(rawData);
-    	System.out.println("Decrypted Message : " + decryptedMessage);
-    	byte[] data = senderNumber.getBytes();
     	
-//    	Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
-//    	Message message = Message.creator(
-//    				new PhoneNumber(senderNumber), 
-//    				new PhoneNumber(receiverNumber),
-//    				"This is automatic message with money: " + amount).create();
+		KeyPair pair = CryptoUtil.GenerateKeys();
+		
+		PrivateKey privateKey = pair.getPrivate();
+	
+    	byte[] dataWithDigitalSignature = CryptoUtil.makeDigitalSignature(stringToEncrypt, privateKey);
+    	byte[] encryptedData = CryptoUtil.encryptDataWithPublicKey(dataWithDigitalSignature, certificate);
+    	
+    	
+    	System.out.println("Encrypted Message : " + new String(encryptedData));
+    	PublicKey publicKey = pair.getPublic();
+    	BankController bank = BankController.getInstance();
+    	bank.processMessage(encryptedData, publicKey);
+    	 
     	
     }
     
-    public static byte[] encryptData(byte[] data, X509Certificate encryptionCertificate) throws CertificateEncodingException, CMSException, IOException {
-	    byte[] encryptedData = null;
-	    if (null != data && null != encryptionCertificate) {
-	    	CMSEnvelopedDataGenerator generator = new CMSEnvelopedDataGenerator();
-	        JceKeyTransRecipientInfoGenerator jceKey = new JceKeyTransRecipientInfoGenerator(encryptionCertificate);
-	        generator.addRecipientInfoGenerator(jceKey);
-	        CMSTypedData msg = new CMSProcessableByteArray(data);
-	        OutputEncryptor encryptor = new JceCMSContentEncryptorBuilder(CMSAlgorithm.AES128_CBC).setProvider("BC").build();
-	        CMSEnvelopedData cmsEnvelopedData = generator.generate(msg,encryptor);
-	        encryptedData = cmsEnvelopedData.getEncoded();
-	    }
-	    return encryptedData;
-	}
     
-    public static byte[] decryptData(byte[] encryptedData, PrivateKey decryptionKey) throws CMSException {
-	    byte[] decryptedData = null;
-	    if (null != encryptedData && null != decryptionKey) {
-	        CMSEnvelopedData envelopedData = new CMSEnvelopedData(encryptedData);
-	        @SuppressWarnings("unchecked")
-			Collection<RecipientInformation> recipients = envelopedData.getRecipientInfos().getRecipients();
-	        KeyTransRecipientInformation recipientInfo  = (KeyTransRecipientInformation) recipients.iterator().next();
-	        JceKeyTransRecipient recipient = new JceKeyTransEnvelopedRecipient(decryptionKey);
-	        return recipientInfo.getContent(recipient);
-	    }
-	    return decryptedData;
-	}
+    
+    public void sendMessageOverNetwork(byte[] data) throws IOException {
+		URL url = new URL("NãoSeiQualURLDoBanco");
+		//Request for establishing the connection
+		URLConnection conn = url.openConnection();
+		conn.setRequestProperty("Content-type", "application/json");
+		conn.setDoOutput(true);
+
+		conn.setConnectTimeout(5000); // Connection timeout to 5 seconds
+		
+		// Open Stream to write (If connection is unsuccessful -> Exception)
+		OutputStream os = conn.getOutputStream();
+		OutputStream out = new BufferedOutputStream(os);
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out));
+		
+        bw.close();
+        out.close();
+        os.close();
+
+        // Wait for response here
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+		String jsonText = "";
+		while ((line = rd.readLine()) != null) {
+            jsonText += line;
+        }
+        rd.close();
+    }
+    
+
     
 }
--- src/main/java/Crypto/CryptoUtil.java
+++ src/main/java/Crypto/CryptoUtil.java
@@ -1,11 +1,14 @@
 package Crypto;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.MessageDigest;
@@ -13,12 +16,16 @@ import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.SecureRandom;
 import java.security.Signature;
 import java.security.SignatureException;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.Certificate;
+import java.security.cert.CertificateEncodingException;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.Collection;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -27,16 +34,38 @@ import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 
+import org.bouncycastle.cms.CMSAlgorithm;
+import org.bouncycastle.cms.CMSEnvelopedData;
+import org.bouncycastle.cms.CMSEnvelopedDataGenerator;
+import org.bouncycastle.cms.CMSException;
+import org.bouncycastle.cms.CMSProcessableByteArray;
+import org.bouncycastle.cms.CMSSignedDataGenerator;
+import org.bouncycastle.cms.CMSTypedData;
+import org.bouncycastle.cms.KeyTransRecipientInformation;
+import org.bouncycastle.cms.RecipientInformation;
+import org.bouncycastle.cms.jcajce.JcaSignerInfoGeneratorBuilder;
+import org.bouncycastle.cms.jcajce.JceCMSContentEncryptorBuilder;
+import org.bouncycastle.cms.jcajce.JceKeyTransEnvelopedRecipient;
+import org.bouncycastle.cms.jcajce.JceKeyTransRecipient;
+import org.bouncycastle.cms.jcajce.JceKeyTransRecipientInfoGenerator;
+import org.bouncycastle.jce.ECNamedCurveTable;
+import org.bouncycastle.jce.spec.ECParameterSpec;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.bouncycastle.operator.OutputEncryptor;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
+import org.bouncycastle.operator.jcajce.JcaDigestCalculatorProviderBuilder;
+
 public class CryptoUtil {
 		
-		private static final String ASYM_CIPHER = "RSA/ECB/PKCS1Padding";
-		private static final String SYM_CIPHER = "AES/CBC/PKCS5Padding";
-		private static final String HASH_FUNCTION = "SHA256";
+		public static final String ASYM_CIPHER = "RSA/ECB/PKCS1Padding";
+		public static final String SYM_CIPHER = "AES/CBC/PKCS5Padding";
+		public static final String HASH_FUNCTION = "SHA256";
 
 		
 		/** Digital signature algorithm. */
-		private static final String SIGNATURE_ALGO = "SHA256withECDSA";
-		private static final String PROVIDER = "BC";
+		public static final String SIGNATURE_ALGO = "SHA256withECDSA";
+		public static final String BOUNCY_CASTLE_PROVIDER = "BC";
 		
 		public static byte[] asymCipher(byte[] plainBytes, Key publicKey) throws NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException{
 			Cipher cipher = Cipher.getInstance(ASYM_CIPHER);
@@ -66,18 +95,26 @@ public class CryptoUtil {
 		}
 		
 		
-		public static boolean verifyDigitalSignature(byte[] cipherDigest, byte[] bytes, PublicKey publicKey) throws NoSuchAlgorithmException, InvalidKeyException, NoSuchProviderException{
+		public static boolean verifyDigitalSignature(byte[] data, byte[] signature, PublicKey publicKey) throws NoSuchAlgorithmException, InvalidKeyException, NoSuchProviderException{
 
 			// verify the signature with the public key
-			Signature ecdsaVerify = Signature.getInstance(SIGNATURE_ALGO,PROVIDER);
+			Signature ecdsaVerify = Signature.getInstance(SIGNATURE_ALGO, BOUNCY_CASTLE_PROVIDER);
 			ecdsaVerify.initVerify(publicKey);
 			try {
-				ecdsaVerify.update(bytes);
-				return ecdsaVerify.verify(cipherDigest);
+				ecdsaVerify.update(data);
+				return ecdsaVerify.verify(signature);
 			} catch (SignatureException se) {
 				System.err.println("Caught exception while verifying " + se);
 				return false;
 			}
+		}
+		
+		public static KeyPair GenerateKeys()
+		    throws NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException {
+		    ECParameterSpec ecSpec = ECNamedCurveTable.getParameterSpec("B-571");
+		    KeyPairGenerator g = KeyPairGenerator.getInstance("ECDSA", BOUNCY_CASTLE_PROVIDER);
+		    g.initialize(ecSpec, new SecureRandom());
+		    return g.generateKeyPair();
 		}
 		
 		/** Calculates digital signature from text. 
@@ -85,13 +122,17 @@ public class CryptoUtil {
 		public static byte[] makeDigitalSignature(byte[] bytes, PrivateKey privatekey) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException, NoSuchProviderException {
 
 			// get a signature object and sign the plain text with the private key
-			Signature ecdsaSign = Signature.getInstance(SIGNATURE_ALGO, PROVIDER);
+			Signature ecdsaSign = Signature.getInstance(SIGNATURE_ALGO, BOUNCY_CASTLE_PROVIDER);
 			ecdsaSign.initSign(privatekey);
 			ecdsaSign.update(bytes);
 			byte[] signature = ecdsaSign.sign();
 			return signature;
 		}
-		
+	    public static X509Certificate getCertificate() throws CertificateException, NoSuchProviderException, FileNotFoundException {
+	    	CertificateFactory certFactory = CertificateFactory.getInstance("X.509", BOUNCY_CASTLE_PROVIDER);
+			X509Certificate certificate = (X509Certificate) certFactory.generateCertificate(new FileInputStream("Certificate.cer"));
+			return certificate;    	
+	    }
 		public static Certificate getX509CertificateFromStream(InputStream in) throws CertificateException {
 			try {
 				CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
@@ -101,6 +142,7 @@ public class CryptoUtil {
 				closeStream(in);
 			}
 		}
+		
 		
 		public static Certificate getX509CertificateFromResource(String certificateResourcePath)
 				throws IOException, CertificateException {
@@ -168,6 +210,47 @@ public class CryptoUtil {
 				throws KeyStoreException {
 			InputStream is = getResourceAsStream(keyStoreResourcePath);
 			return readKeystoreFromStream(is, keyStorePassword);
+		}
+	    public static byte[] encryptDataWithPrivateKey(byte[] data, X509Certificate encryptionCertificate, PrivateKey privateKey) throws CertificateEncodingException, CMSException, IOException, OperatorCreationException {
+		    byte[] encryptedData = null;
+		    if (null != data && null != encryptionCertificate) {
+		    	
+		    	  CMSSignedDataGenerator gen = new CMSSignedDataGenerator();
+		          CMSTypedData msg = new CMSProcessableByteArray(data);
+		    	  ContentSigner shaSigner = new JcaContentSignerBuilder("SHA1withECDSA").setProvider(CryptoUtil.BOUNCY_CASTLE_PROVIDER).build(privateKey);
+		    	  gen.addSignerInfoGenerator(new JcaSignerInfoGeneratorBuilder(
+		    	    new JcaDigestCalculatorProviderBuilder().setProvider("BC").build())
+		    	    .build(shaSigner, encryptionCertificate));
+		    	  return gen.generate(msg, false).getEncoded();
+		    }
+		    return encryptedData;
+		}
+	    public static byte[] encryptDataWithPublicKey(byte[] data, X509Certificate encryptionCertificate) throws CertificateEncodingException, CMSException, IOException {
+		    byte[] encryptedData = null;
+		    if (null != data && null != encryptionCertificate) {
+		    			    	    
+		    	CMSEnvelopedDataGenerator generator = new CMSEnvelopedDataGenerator();
+		        JceKeyTransRecipientInfoGenerator jceKey = new JceKeyTransRecipientInfoGenerator(encryptionCertificate);
+		        generator.addRecipientInfoGenerator(jceKey);
+		        CMSTypedData msg = new CMSProcessableByteArray(data);
+		        OutputEncryptor encryptor = new JceCMSContentEncryptorBuilder(CMSAlgorithm.AES128_CBC).setProvider(CryptoUtil.BOUNCY_CASTLE_PROVIDER).build();
+		        CMSEnvelopedData cmsEnvelopedData = generator.generate(msg,encryptor);
+		        encryptedData = cmsEnvelopedData.getEncoded();
+		    }
+		    return encryptedData;
+		}
+	    
+	    public static byte[] decryptDataWithPrivateKey(byte[] encryptedData, PrivateKey decryptionKey) throws CMSException {
+		    byte[] decryptedData = null;
+		    if (null != encryptedData && null != decryptionKey) {
+		        CMSEnvelopedData envelopedData = new CMSEnvelopedData(encryptedData);
+		        @SuppressWarnings("unchecked")
+				Collection<RecipientInformation> recipients = envelopedData.getRecipientInfos().getRecipients();
+		        KeyTransRecipientInformation recipientInfo  = (KeyTransRecipientInformation) recipients.iterator().next();
+		        JceKeyTransRecipient recipient = new JceKeyTransEnvelopedRecipient(decryptionKey);
+		        return recipientInfo.getContent(recipient);
+		    }
+		    return decryptedData;
 		}
 }
 
